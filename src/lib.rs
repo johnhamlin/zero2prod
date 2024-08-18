99c3892b5bb1885bbe324fbaff7ce7724f52bb05
John Hamlin <johnhamlin@gmail.com>
2024-08-18 13:55:58 -0400

    Add postgres db

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,7 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+pub const LOCAL_HOST: &str = "http://127.0.0.1";
+pub const DEV_PORT: &str = "8000";
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run();
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
